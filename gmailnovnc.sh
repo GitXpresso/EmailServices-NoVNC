a6e0a09bfad165b3f106c96930e3130a713a6045
GitXpresso <gitxpresso@outlook.com>
2025-01-19 22:09:36 +0000

    Automatically changes directory to gmail-desktop

--- gmailnovnc.sh
+++ gmailnovnc.sh
@@ -4,10 +4,11 @@ sudo apt update
 echo "installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
 cd ~/
-wget https://github.com/timche/gmail-desktop/releases/download/v2.25.3/gmail-desktop-2.25.3-linux.tar.bz2 && tar -xvf gmail-desktop-2.25.3-linux.tar.bz2 -C ~/ && sudo rm -rf gmail-desktop-2.25.3-linux.tar.bz2
+wget https://github.com/timche/gmail-desktop/releases/download/v2.25.3/gmail-desktop-2.25.3-linux.tar.bz2 && tar -xvf gmail-desktop-2.25.3-linux.tar.bz2 -C ~/ && sudo rm -rf gmail-desktop-2.25.3-linux.tar.bz2 && sudo mv -f gmail-desktop-2.25.3-linux gmail-desktop
 git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
-~/gmail-desktop-2.25.3-linux/gmail-desktop --display=:0
+sudo rm -rf /home/codeany/gmail-desktop-2.25.3-linux
+~/gmail-desktop/gmail-desktop --display=:0
 echo -e "Gmail Desktop and NoVNC has started go to https://localhost:6080 to access NoVNC and Gmail Desktop"
