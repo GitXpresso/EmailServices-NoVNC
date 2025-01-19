1280e06314c853d4030bf600d8ee7a597726a37a
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 17:51:58 -0500

    Removed unwanted command

--- gmailnovnc.sh
+++ gmailnovnc.sh
@@ -9,6 +9,5 @@ git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
-sudo rm -rf /home/codeany/gmail-desktop-2.25.3-linux
 ~/gmail-desktop/gmail-desktop --in-process-gpu --display=:0
 echo -e "Gmail Desktop and NoVNC has started go to https://localhost:6080 to access NoVNC and Gmail Desktop"
