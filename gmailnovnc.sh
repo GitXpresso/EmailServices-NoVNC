e81ae2efd2ac78a2a829d98892d2a871cebc1f96
GitXpresso <gitxpresso@outlook.com>
2025-01-19 22:05:22 +0000

    Finished GmailNoVNC.sh

--- gmailnovnc.sh
+++ gmailnovnc.sh
@@ -8,4 +8,6 @@ wget https://github.com/timche/gmail-desktop/releases/download/v2.25.3/gmail-des
 git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-/usr/lib/zen/zen --display=:0
+export DISPLAY=:0
+/home/codeany/gmail-desktop-2.25.3-linux/gmail-desktop --display=:0
+echo -e "Gmail Desktop and NoVNC has started go to https://localhost:6080 to access NoVNC and Gmail Desktop"
