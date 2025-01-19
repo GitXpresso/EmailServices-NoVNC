32ee5afb1b3c8f572fb1bd5c0abc9f462d7f73be
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 16:43:10 -0500

    Update gmailnovnc.sh

--- gmailnovnc.sh
+++ gmailnovnc.sh
@@ -1,1 +1,11 @@
-wget https://github.com/timche/gmail-desktop/releases/download/v2.25.3/gmail-desktop-2.25.3-linux.tar.bz2 && sudo tar -xvf gmail-desktop-2.25.3-linux.tar.bz2 -C ~/ && sudo rm -rf gmail-desktop-2.25.3-linux.tar.bz2   
+#!/bin/bash
+echo "updating system to successfully install required packages"
+sudo apt update
+echo "installing required packages"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
+cd ~/
+wget https://github.com/timche/gmail-desktop/releases/download/v2.25.3/gmail-desktop-2.25.3-linux.tar.bz2 && sudo tar -xvf gmail-desktop-2.25.3-linux.tar.bz2 -C ~/ && sudo rm -rf gmail-desktop-2.25.3-linux.tar.bz2
+git clone https://github.com/gitxpresso/linux-novnc.git
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+/usr/lib/zen/zen --display=:0
