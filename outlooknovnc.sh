93b14cf4af586ae97cd9fddc2be5ec24f17926e7
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 17:46:49 -0500

    Update outlooknovnc.sh

--- outlooknovnc.sh
+++ outlooknovnc.sh
@@ -1,1 +1,1 @@
-wget https://github.com/julian-alarcon/prospect-mail/releases/download/v1.3.13/prospect-mail_1.3.13_arm64.deb && sudo apt install -y ./prospect-mail_1.3.13_arm64.deb
+wget https://github.com/julian-alarcon/prospect-mail/releases/download/v1.3.13/outlook-for-linux-1.3.13.tar.gz && tar -xvf outlook-for-linux-1.3.13.tar.gz -C ~/ && sudo rm -rf outlook-for-linux-1.3.13.tar.gz
